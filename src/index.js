ec22c0ffa2c4bd253b0b1193b7b7bfd20f3b12b5
eva montgomery <eva.m.montgomery@gmail.com>
2020-02-04 13:32:44 -0500

    adds simple test to make sure we are getting data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+// Ajax = 
+// Asynchronos
+// JavaScript
+// And 
+// XML
+
+import axios from 'axios';
+const API_ENDPOINT = 'https://swapi.co/api/people/1/';
+
+axios.get(API_ENDPOINT)
+    .then(response => {
+        console.log(response);
+    })
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
